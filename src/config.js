64c00b2bca47976b485e1f3b334175b2572ec673
Juan José Nolasco <nolascomeza18@gmail.com>
2018-01-29 22:05:18 -0700

    connect to database

---
+++ src/config.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyB6YGqA5lPKgFlNKbcoQ-9PvHNIZ1wKU5w",
+    authDomain: "firechat-8ca4e.firebaseapp.com",
+    databaseURL: "https://firechat-8ca4e.firebaseio.com",
+    projectId: "firechat-8ca4e",
+    storageBucket: "firechat-8ca4e.appspot.com",
+    messagingSenderId: "696566111689"
+  };
+
+  export default config 
---
+++ src/initialize.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="init">Initialize app base state</button>
+        <button @click="get">Get</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InitializeData',
+    methods: {
+        init () {
+            this.$store.dispatch('users/seed')
+            this.$store.dispatch('conversations/seed')
+        },
+        get () {
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
