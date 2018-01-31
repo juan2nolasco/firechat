ca5499cd45781c90e2d2b0228401eb8cf9e000e1
Juan José Nolasco <nolascomeza18@gmail.com>
2018-01-30 22:58:57 -0700

    display firestore data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
 <div>
   <Initialize />
+  <ConversationContainer
+    v-for="id in convoIds"
+    :conversation="conversations[id]"
+    :id="id"
+    :key="id"
+  />
 </div>
 </template>
 
 <script>
 import Initialize from './Initialize.vue'
+import ConversationContainer from './ConversationContainer.vue'
+
+import { mapState } from 'vuex'
 
 export default {
   name: 'app',
@@ -13,8 +22,15 @@ export default {
       console.log(this.$store.state.db)
     },
     components: {
-      Initialize
-    }
+      Initialize,
+      ConversationContainer
+    },
+    computed: {
+      ...mapState({
+        conversations: state => state.conversations.all,
+        convoIds: state => state.conversations.allIds
+      })
+    },
 }
 </script>
 
